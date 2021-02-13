8bf880526200b506db86c0b9f3cf545758d44f21
Oussama B.Sassi <obensassi.03@gmail.com>
2021-02-13 12:06:04 +0100

    Remove unused variable

--- ConnectedDevices.py
+++ ConnectedDevices.py
@@ -5,12 +5,11 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 host = s.getsockname()[0]
-#print(host)
+#print("host",host)
 s.close()
 nm = nmap.PortScanner()
 l = nm.scan(hosts = f'{host}/24', arguments = '-sn')
 p = l['scan'].keys()
 p = list(p)
-LenList = len(p)
-for i in range(LenList):
+for i in range(len(p)):
     print("IP",i,p[i])
